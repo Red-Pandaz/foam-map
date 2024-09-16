3406fd7cfa6ea47992ac51c8a634a545731b7ddd
Red Panda <Deep_Sea_Penguin@protonmail.com>
2024-09-16 19:14:06 -0400

    more server adjustments

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ async function startServer() {
     const app = express();
     app.use(helmet());
 
-    // Set up Content Security Policy (CSP) using Helmet middleware
     app.use(
         helmet.contentSecurityPolicy({
             directives: {
@@ -22,50 +21,34 @@ async function startServer() {
                 'script-src': ["'self'", "https://api.mapbox.com"],  // Allow Mapbox GL script
                 'style-src': ["'self'", "'unsafe-inline'", "https://api.mapbox.com"], // Allow Mapbox GL stylesheet
                 'img-src': ["'self'", 'data:', 'blob:'], // Allow images from self, data URIs, and blob URIs
-                // ... other directives
-                // 'script-src': ["'self'", "https://dune.com"],
-                // 'style-src': ["'self'", "'unsafe-inline'", "https://dune.com"],
-                // 'img-src': ["'self'", 'data:', 'blob:'],
-                // 'frame-src': ["'self'", "https://dune.com"],
-                // 'child-src': ["'self'", 'blob:'],
                 'connect-src': [
                     "'self'",
-                    // "https://optimism.io",
+                    "https://optimism.io",
                     "https://*.tiles.mapbox.com",
                     "https://api.mapbox.com",
                     "https://events.mapbox.com",
                     "https://static.optimism.io",
                     "https://dune.com",
                     "http://localhost:3000",
-                    "http://localhost:*"
-                
-                    // "http://www.foamcaster.xyz",
-                    // "http://foamcaster.xyz",
-                    // "https://www.foamcaster.xyz",
-                    // "https://foamcaster.xyz",
-                    // "http://foamcaster.xyz:3000/token",
-                    // "http://foamcaster.xyz:3000/api/markers",
-                    // "http://foamcaster.xyz:*",
-                    // "https://foamcaster.xyz:*"
+                    "https://www.foamcaster.xyz",
+                    "https://foamcaster.xyz"
                 ],
                 'worker-src': ["'self'", 'blob:'],
                 'child-src': ["'self'", 'blob:']
-                // Add other directives as needed
             }
         })
     );
 
-    // Set up rate limiting middleware for the root endpoint
+
     const limiterForRootEndpoint = rateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 100, // Limit each IP to 100 requests per windowMs
         message: 'Too many requests from this IP, please try again later.',
     });
-    // Apply rate limiting middleware to the root endpoint
     app.use('/', limiterForRootEndpoint);
     app.set('trust proxy', 1);
 
-    // Fetch the Mapbox token from Google Secret Manager
+
     let mapboxToken;
     try {
         mapboxToken = await retryApiCall(() => accessSecret('MAPBOX_API'));
@@ -77,10 +60,8 @@ async function startServer() {
     // Middleware setup
     const allowedOrigins = [
         'http://localhost:3000',
-        // 'http://www.foamcaster.xyz',
-        // 'http://foamcaster.xyz',
-        // 'https://www.foamcaster.xyz',
-        // 'https://foamcaster.xyz'
+        'https://www.foamcaster.xyz',
+        'https://foamcaster.xyz'
     ];
 
     app.use(cors({
@@ -95,45 +76,36 @@ async function startServer() {
         }
     }));
 
-    // Serve static files from the 'public' directory
+
     app.use(express.static(path.join(__dirname, 'public')));
 
-    // Token endpoint
+
     app.get('/token', (req, res) => {
         res.json({ token: mapboxToken });
     });
 
-    // API routes
+
     app.use('/api', router);
 
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use((err, req, res, next) => {
+        console.error('Error occurred:', err.message);
+        res.status(500).send('Internal Server Error');
+    });
 
-    // Start the server
-    const PORT = process.env.PORT || 3000;
-    try {
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error('Error starting server:', error);
-        process.exit(1); // Exit process with an error code
-    }
 
-    // Configure HTTPS options
     const httpsOptions = {
         key: fs.readFileSync('/etc/letsencrypt/live/foamcaster.xyz/privkey.pem'),
         cert: fs.readFileSync('/etc/letsencrypt/live/foamcaster.xyz/fullchain.pem'),
     };
 
-    // Create HTTPS server
-    const httpsServer = https.createServer(httpsOptions, app);
 
-    // Start the HTTPS server
     const HTTPS_PORT = process.env.HTTPS_PORT || 8443;
+    const httpsServer = https.createServer(httpsOptions, app);
+    
     httpsServer.listen(HTTPS_PORT, () => {
         console.log(`HTTPS server running on port ${HTTPS_PORT}`);
     });
 }
 
-// Call the async function to start the server
+
 startServer();
